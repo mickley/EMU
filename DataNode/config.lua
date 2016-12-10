e31e78635b33e0cb888d3edbb77a4ff92bf3d94f
James Mickley <mickley@gmail.com>
2016-12-09 20:33:12 -0500

    Improved config.lua
    Organized by section, added thingspeak API and option to sync time over SNTP when connected to wifi.

--- DataNode/config.lua
+++ DataNode/config.lua
@@ -22,11 +22,24 @@ Replace the EDITME sections with your own information
 - 11/16/2016 JGM - Version 1.4: 
     - Added hostname and removed thingspeak API key
 
+- 12/9/2016 JGM - Version 1.5:
+    - Added thingspeak API key again, and an option to specify whether to sync 
+      time over SNTP when connected to wifi
+
 --]]
 
 
 -- ########### Configuration ###########
 -- Set configuration variables here
+
+
+-- ##### Startup File #####
+
+-- Set startup file to execute by default
+startup = "startupfile.lua"
+
+
+-- ##### Wifi Options #####
 
 -- WIFI SSID (network name)
 wifi_ssid = "EDITME"
@@ -34,7 +47,7 @@ wifi_pass = "EDITME"
 
 -- Is Wifi required for startup routine?
 -- If so, then init.lua will try to wait until connected before running startup
-wifi_required = true
+wifi_required = false
 
 -- Set the router IP address and the IP address you want the ESP to have
 -- This speeds up connecting when trying to save power
@@ -45,14 +58,11 @@ wifi_router = nil
 -- If running a webserver, you can then go to http://hostname
 wifi_hostname = nil
 
--- Set startup file to execute by default
-startup = "test.lua"
 
---Interval to sleep in seconds
-interval = (15 * 60)
+-- ##### Time Options #####
 
--- Hours relative to UTC
-timezone = -4
+-- Sync the time via sntp when connected to wifi
+wifi_sync_time = true
 
 -- Time sync ntp server
 -- Lots of options here, find the best one
@@ -61,8 +71,23 @@ timeserver1 = "us.pool.ntp.org"
 timeserver2 = "pool.ntp.org"
 -- 1.us.pool.ntp.org
 
+-- Hours relative to UTC
+timezone = -5
+
+--Interval to sleep in minutes
+interval = 15
+
+
+-- ##### Miscellany #####
+
 -- adc.readvdd33() is low by about 0.25V for D1 Mini
 volt_adj = 0.25
+
+-- Add the thingspeak API key to use
+thingspeak_API = "EDITME"
+
+
+-- ##### Configuration Finished #####
 
 print("Configuration settings loaded")
 -- #####################################
