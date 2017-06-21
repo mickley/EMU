8da41dcdbe5fdd251dbc1dd08d6d75c510e529ed
James Mickley <mickley@gmail.com>
2017-06-21 15:25:16 -0400

    Data processor for EMU fenton data + ibuttons

---
+++ EMU-Analysis/Fenton-data-processor.R
@@ -0,0 +1,150 @@
+##### EMU Data Processor Script for Fenton data #####
+# This takes a folder full of EMU, hobo, & iButton data files and makes 
+# a composite data file
+#
+# It converts timestamps to text date/time and adds site/transect data
+#
+#####################################
+
+
+# Load libraries required to data wrangle
+require(tidyr)
+require(dplyr)
+require(lubridate)
+
+
+#### iButton Data ####
+    
+# Data folder
+datafolder = "Data/EMU-6-21/"
+
+# Read in the iButton data
+ibuttons <- read.csv("Data/EMU-6-21/ibuttons-combined.csv") %>%
+
+    # Add the source column and convert the timestamp to POSIXct class
+    mutate(source = "iButton", 
+        timestamp = as.POSIXct(timestamp, tz = "America/New_York"))
+
+
+#### Hobo Data ####
+
+
+
+
+
+#### EMU Data ####
+
+
+# Get a list of data files from the folder
+emus <- list.files(path = datafolder, pattern = "*-data.csv")
+
+# Make an empty dataset
+data = data.frame()
+    
+# Process each data file one at a time
+for (file in emus) {
+
+    # Get the EMU name from the filename
+    name = strsplit(file, "-")[[1]][1]
+    
+    # Read the datafile in from csv
+    tmp <- read.csv(paste(datafolder, file, sep = "")) %>% 
+        
+        # Add a column for the emu name
+        mutate(emu = name)
+    
+    # append this emu's data onto the dataset
+    data <- rbind(data, tmp)
+
+}
+ 
+# Read in the site data
+sites <- read.csv("Data/emu-sitedata-v2.csv", stringsAsFactors = F)
+
+
+##### Combine Datasets & Wrangle #####
+
+
+# Manipulate the data
+data <- data %>%
+    
+    # Add source column
+    mutate(source = "EMU") %>%
+    
+    # Convert timestamp to text date
+    mutate(Timestamp = as.POSIXct(Timestamp, origin = "1970-01-01", 
+        tz = "America/New_York")) %>%
+        
+    # Convert column names to lowercase
+    `names<-`(tolower(names(.))) %>%
+    
+    # Join the emu and site specific data
+    left_join(sites, by = "emu") %>%
+    
+    # Add vwc based on good sand calibration
+    mutate(vwc = 6.552253e-01 + -4.955260e-05 * soil + 
+        9.539485e-10 * soil ^ 2) %>%
+
+    # Add par based on LiCor calibration
+    mutate(par = 0.01405 * light + 33.4) %>%
+
+    # Add iButton data
+    bind_rows(ibuttons) %>%
+
+    # Add additional date/time columns
+    mutate(
+        # Days since start
+        day = floor(interval(as.POSIXct("2017-05-30"), timestamp) / ddays(1)),
+        
+        # Hours since start
+        hour = floor(interval(as.POSIXct("2017-05-30 12:00"), timestamp) / dhours(1)),
+        
+        # Minutes since start
+        minute = floor(interval(as.POSIXct("2017-05-30 12:00"), timestamp) / dminutes(1)),
+        
+        # Hour of the day
+        day.hr = hour(timestamp),
+        
+        # Minute of the day
+        day.min = minute(timestamp) + (day.hr * 60)
+        ) %>%
+
+        
+    # Reorder columns
+    select(site, transect, order, lat, long, source, emu, timestamp, day, hour, 
+           minute, day.hr, day.min, temperature, 
+           humidity, vwc, par, light, soil, pressure, voltage) %>%
+            
+    # Filter out all data points that aren't on a 15-minute interval
+    filter(minute(timestamp) == 0 | minute(timestamp) == 15 | 
+            minute(timestamp) == 30 | minute(timestamp) == 45) %>%
+    
+    # Filter out all data before the start of the respective sites
+    filter(
+        
+        # Start time for the two Fenton transects
+        (site == "Fenton" & 
+            timestamp >= as.POSIXct("2017-05-30 13:30"))
+            
+        # Start time for the Fenton transects
+        #(site == "Fenton" & 
+        #    timestamp >= as.POSIXct("2017-04-15 14:45")) |
+            
+        # Start time for the third HEEP transect
+        #(site == "HEEP" & transect == "Forest" & 
+        #    timestamp >= as.POSIXct("2017-05-02 10:00"))
+        
+        ) %>%
+            
+    arrange(site, transect, order, source, timestamp)
+    
+
+str(data)
+
+
+# Write out data to csv
+#write.csv(data, file = paste(datafolder, "alldata.csv", sep = ""))
+
+# Write out a message
+#cat(paste("Datafiles processed to: ", datafolder, "alldata.csv", sep = ""))
+
